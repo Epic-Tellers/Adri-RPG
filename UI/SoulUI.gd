1b9605becd68b1be170399eee55f54c4840890ab
Adrià Serrano López <47743962+adriaserrano97@users.noreply.github.com>
2022-09-27 16:30:21 +0200

    Flair + bugfix

--- UI/SoulUI.gd
+++ UI/SoulUI.gd
@@ -1,20 +1,17 @@
 extends Control
 
-var soulDisplayed = 0
-
 func _ready():
 	if PlayerSaveInfo.connect("added_batsoul",self,"_on_added_batsoul") != OK:
 		print("Error in SoulUI trying to connect PlayerSaveInfo's added_batsoul signal to _on_added_batsoul method")
-	soulDisplayed = int(soulDisplayed)
-	$Label.text = str(soulDisplayed)
+	update_soul_displayed()
 
 func _on_added_batsoul():
-	update_soul_displayed()
 	var TW = create_tween()
 	#TW.tween_property(self,"speed",TARGET_SPEED,1.0)
 	TW.tween_property($Icon,"rect_scale",Vector2(1.4,1.4),0.15)
 	TW.tween_property($Icon,"rect_scale",Vector2(1.0,1.0),0.05)
-	$Label.text = str(soulDisplayed)
+	yield(TW, "finished")
+	update_soul_displayed()
 	
 func update_soul_displayed():
-	soulDisplayed = PlayerStats.batSoulsThisRun
+	$Label.text = str(PlayerStats.batSoulsThisRun)
