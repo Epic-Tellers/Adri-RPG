134d2b6694aeac30b2c0cdbfefb63f3366ff734f
Adrià Serrano López <47743962+adriaserrano97@users.noreply.github.com>
2022-09-27 16:59:19 +0200

    Lil counter

--- Player/UpgradesMenu.gd
+++ Player/UpgradesMenu.gd
@@ -1,9 +1,14 @@
 extends Control
 
 signal back_pressed
+onready var batSoulLabel = $Panel/MainScreen/Holder/Icon/Label
 
 func _ready():
-	pass # Replace with function body.
+	update_batsoul_label()
+	$Panel/MainScreen/Holder/BackButton.grab_focus()
 
 func _on_BackButton_pressed():
 	emit_signal("back_pressed")
+
+func update_batsoul_label():
+	batSoulLabel.text = str(PlayerSaveInfo.batSouls)
